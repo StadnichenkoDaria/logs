3e15d91c8305517be08d62e76663bf97a64e8f66
StadnichenkoDaria <brodiagad@gmail.com>
2022-02-21 19:47:57 +0300

    logs v2

--- parser.py
+++ parser.py
@@ -2,7 +2,6 @@ import argparse
 import re
 import os
 import json
-import subprocess
 from collections import defaultdict
 
 parser = argparse.ArgumentParser()
@@ -17,22 +16,17 @@ dict_ip = defaultdict(
     lambda: {"GET": 0, "POST": 0, "PUT": 0, "DELETE": 0, "HEAD": 0}
 )
 
-# todo: Должна быть возможность обработки всех логов внутри одного директория (статистика по каждому логу
-#       должна выводиться отдельно и должна быть сохранена в отдельном json-файле)
-if os.path.isfile(args.file):
-    with open(args.file) as file:
-        # вот здесь мне не нравится передавать access.log, мне бы хотелось передать переменную file,
-        # но я не нашла как сделать, чтобы os.system или os.popen ее принимало,
-        # переменные принимает subprocess, но он не проглотил такую сложную строку
-        cmd_top_requests = 'tail -n 10 access.log | awk \'{print $1}\' | uniq -ci | sort -nr | head -n 3'
-        cmd_top_long_requests = 'tail -n 10 access.log | awk \'{print $6, $11, $1, $NF, $4}\' | sort -nrk4 | head -n 3'
+
+def process_file(log):
+    with open(log) as file:
+        cmd_top_requests = 'cat %s | awk \'{print $1}\' | uniq -ci | sort -nr | head -n 3' % log
+        cmd_top_long_requests = 'cat %s | awk \'{print $6, $11, $1, $NF, $4}\' | sort -nrk4 | head -n 3' \
+                                % log
         out_top_requests = os.popen(cmd_top_requests).read().split('\n')
         out_top_long_requests = os.popen(cmd_top_long_requests).read().split('\n')
 
         idx = 0
         for line in file:
-            if idx > 99:
-                break
             idx += 1
             total_requests = idx
             ip_match = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line)
@@ -42,17 +36,18 @@ if os.path.isfile(args.file):
                 if method is not None:
                     dict_ip[ip][method.group(1)] += 1
                     idx += 1
+    dict_ip["Amount requests"] = total_requests
+    dict_ip["TOP REQUESTS"] = out_top_requests
+    dict_ip["TOP LONG REQUESTS"] = out_top_long_requests
+    print(json.dumps(dict_ip, indent=4))
+
+    with open('access_log.json', 'w') as f:
+        f.write(json.dumps(dict_ip))
+
+
+if os.path.isfile(args.file):
+    process_file(args.file)
 
 elif os.path.isdir(args.file):
-    directory = subprocess.run(['find', args.file, '-name', '*.log'])
-    # тут по идее, наверное надо итерироваться по списку файлов и с каждым делать то же самое,
-    # но TypeError: 'CompletedProcess' object is not iterable
-    # и у меня пока нет идей, как его сделать итерируемым
-
-dict_ip["Amount requests"] = total_requests
-dict_ip["TOP REQUESTS"] = out_top_requests
-dict_ip["TOP LONG REQUESTS"] = out_top_long_requests
-print(json.dumps(dict_ip, indent=4))
-
-with open('access_log.json', 'w') as f:
-    f.write(json.dumps(dict_ip))
+    for f in os.listdir(args.file):
+        process_file(args.file + f)
